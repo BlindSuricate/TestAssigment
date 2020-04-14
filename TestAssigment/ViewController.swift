3d034602ffed53daf78040f3b41f99819b4fe3a7
Tarasenko Serhii <blindsuricate@gmail.com>
2020-04-14 18:31:42 +0300

    Initial Commit

---
+++ TestAssigment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAssigment
+//
+//  Created by user on 2020-04-14.
+//  Copyright © 2020 TarasenkoSerhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
